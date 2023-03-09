f41c8228efba9c5613f65caa460646fdb5c6f666
apetersontrue <apeterson@truefitness.com>
2023-03-09 15:08:08 -0600

    Add intent to start file browser - IP

--- app/src/main/java/com/example/xgbuddy/SetupLoaderFragment.kt
+++ app/src/main/java/com/example/xgbuddy/SetupLoaderFragment.kt
@@ -1,11 +1,13 @@
 package com.example.xgbuddy
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -41,7 +43,13 @@ class SetupLoaderFragment : Fragment() {
     private fun startFileBrowser(v: View) {
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
             addCategory(Intent.CATEGORY_OPENABLE)
-            type
         }
+        val activityResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    // Figure out how to get data from result
+                }
+            }
+        activityResult.launch(intent)
     }
 }
