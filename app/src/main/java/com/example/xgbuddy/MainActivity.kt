b728d43b9dd769f7bcda6cbaa9d4807c06ef6a1c
apetersontrue <apeterson@truefitness.com>
2023-03-10 13:20:37 -0600

    Migrate MainFragment logic to MainActivity and SetupLoader logic to MainFragment

--- app/src/main/java/com/example/xgbuddy/MainActivity.kt
+++ app/src/main/java/com/example/xgbuddy/MainActivity.kt
@@ -3,20 +3,85 @@ package com.example.xgbuddy
 import android.animation.ValueAnimator
 import android.content.pm.PackageManager
 import android.graphics.drawable.AnimatedVectorDrawable
+import android.media.midi.MidiDeviceInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.ImageView
+import androidx.core.view.MenuProvider
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var midiSession: MidiSession
+
+    private var connectedDevices: Set<MidiDeviceInfo> = setOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (packageManager.hasSystemFeature(PackageManager.FEATURE_MIDI)) {
             setContentView(R.layout.activity_main)
+            setupOptionsMenu()
         } else {
             displayNoMidiCompatScreen()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        midiSession.apply {
+            connectedDeviceList.observe(this@MainActivity) {
+                connectedDevices = it
+                Log.d(TAG, "Devices = ${it.joinToString(" ")}")
+
+                // Open connection status fragment if no inputs or outputs are open
+                if (connectedDevices.isEmpty() || (midiSession.getInputDevices()
+                        .isEmpty() && midiSession.getOutputDevices().isEmpty())
+                ) {
+                    if (supportFragmentManager.findFragmentByTag(ConnectionStatusFragment.TAG) == null) {
+                        ConnectionStatusFragment().show(
+                            supportFragmentManager,
+                            ConnectionStatusFragment.TAG
+                        )
+                    }
+                }
+            }
+            inputDeviceOpened.observe(this@MainActivity) {
+                Log.d(TAG, "InputDeviceOpened = $it")
+            }
+            outputDeviceOpened.observe(this@MainActivity) {
+                Log.d(TAG, "OutputDeviceOpened = $it")
+            }
+        }
+    }
+
+    private fun setupOptionsMenu() {
+        addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.main_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.qs_capture -> {
+                        if (supportFragmentManager.findFragmentByTag(QS300PresetCaptureFragment.TAG) == null) {
+                            QS300PresetCaptureFragment().show(
+                                supportFragmentManager,
+                                QS300PresetCaptureFragment.TAG
+                            )
+                        }
+                        true
+                    }
+                    else -> false
+                }
+            }
+        })
     }
 
     private fun displayNoMidiCompatScreen() {
@@ -33,5 +98,9 @@ class MainActivity : AppCompatActivity() {
             repeatMode = ValueAnimator.REVERSE
             start()
         }
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
--- app/src/main/java/com/example/xgbuddy/MainFragment.kt
+++ app/src/main/java/com/example/xgbuddy/MainFragment.kt
@@ -1,81 +1,37 @@
 package com.example.xgbuddy
 
-import android.media.midi.MidiDeviceInfo
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
-import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import androidx.navigation.fragment.findNavController
+import com.example.xgbuddy.databinding.FragmentMainBinding
 
-@AndroidEntryPoint
 class MainFragment : Fragment() {
 
-    @Inject
-    lateinit var midiSession: MidiSession
-
-    private var connectedDevices: Set<MidiDeviceInfo> = setOf()
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        midiSession.apply {
-            connectedDeviceList.observe(viewLifecycleOwner) {
-                connectedDevices = it
-                Log.d(TAG, "Devices = ${it.joinToString(" ")}")
-
-                // Open connection status fragment if no inputs or outputs are open
-                if (connectedDevices.isEmpty() || (midiSession.getInputDevices()
-                        .isEmpty() && midiSession.getOutputDevices().isEmpty())
-                ) {
-                    if (childFragmentManager.findFragmentByTag(ConnectionStatusFragment.TAG) == null) {
-                        ConnectionStatusFragment().show(
-                            childFragmentManager,
-                            ConnectionStatusFragment.TAG
-                        )
-                    }
-                }
-            }
-            inputDeviceOpened.observe(viewLifecycleOwner) {
-                Log.d(TAG, "InputDeviceOpened = $it")
-            }
-            outputDeviceOpened.observe(viewLifecycleOwner) {
-                Log.d(TAG, "OutputDeviceOpened = $it")
-            }
-        }
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        binding.bNewQSSetup.setOnClickListener(this::createQSSetup)
+        binding.bNewXGSetup.setOnClickListener(this::createXGSetup)
+        binding.bLoadExisting.setOnClickListener(this::startFileBrowser)
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupOptionsMenu()
+    private fun createQSSetup(v: View) {
+
     }
 
-    private fun setupOptionsMenu() {
-        val menuHost: MenuHost = requireActivity()
-        menuHost.addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.main_menu, menu)
-            }
+    private fun createXGSetup(v: View) {
 
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                return when (menuItem.itemId) {
-                    R.id.qs_capture -> {
-                        if (childFragmentManager.findFragmentByTag(QS300PresetCaptureFragment.TAG) == null) {
-                            QS300PresetCaptureFragment().show(
-                                childFragmentManager,
-                                QS300PresetCaptureFragment.TAG
-                            )
-                        }
-                        true
-                    }
-                    else -> false
-                }
-            }
-        })
+    }
+
+    private fun startFileBrowser(v: View) {
+        findNavController().navigate(MainFragmentDirections.actionMainFragmentToFileBrowserFragment())
     }
 
     companion object {
