0ecf49d022d2898582098c1ffaaf45d27cdd8f2f
apetersontrue <apeterson@truefitness.com>
2023-03-30 13:53:53 -0500

    Add notes for sending note messages on key press

--- app/src/main/java/com/example/xgbuddy/ui/KeyboardFragment.kt
+++ app/src/main/java/com/example/xgbuddy/ui/KeyboardFragment.kt
@@ -5,9 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.xgbuddy.MidiSession
 import com.example.xgbuddy.databinding.FragmentKeyboardBinding
+import com.example.xgbuddy.ui.custom.KeyView
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class KeyboardFragment : Fragment() {
+
+    @Inject
+    lateinit var midiSession: MidiSession
 
     private val binding: FragmentKeyboardBinding by lazy {
         FragmentKeyboardBinding.inflate(layoutInflater)
@@ -18,6 +26,22 @@ class KeyboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        for (i in 0 until binding.keysContainer.childCount) {
+            (binding.keysContainer.getChildAt(i) as KeyView).listener =
+                object : KeyView.OnKeyPressListener {
+                    override fun onKeyDown(note: String) {
+                        // Encode note to message
+                        // Create note-on MidiMessage
+                        // midiSession.send(message)
+                    }
+
+                    override fun onKeyUp(note: String) {
+                        // Encode note to message
+                        // Create note-off MidiMessage
+                        // midiSession.send(message)
+                    }
+                }
+        }
         return binding.root
     }
 }
