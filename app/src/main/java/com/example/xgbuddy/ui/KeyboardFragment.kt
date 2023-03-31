39c6e629384634f5b1da77f6770a202315ff9cc9
subhouse07 <subhouse07@gmail.com>
2023-03-31 10:42:26 -0500

    Send note on/off messages from keyboard

--- app/src/main/java/com/example/xgbuddy/ui/KeyboardFragment.kt
+++ app/src/main/java/com/example/xgbuddy/ui/KeyboardFragment.kt
@@ -1,14 +1,19 @@
 package com.example.xgbuddy.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.xgbuddy.MidiSession
+import com.example.xgbuddy.data.MidiMessage
+import com.example.xgbuddy.data.Note
 import com.example.xgbuddy.databinding.FragmentKeyboardBinding
 import com.example.xgbuddy.ui.custom.KeyView
+import com.example.xgbuddy.util.EnumFinder.findBy
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -16,6 +21,9 @@ class KeyboardFragment : Fragment() {
 
     @Inject
     lateinit var midiSession: MidiSession
+
+    private var octave = 5
+    private var channel = 0
 
     private val binding: FragmentKeyboardBinding by lazy {
         FragmentKeyboardBinding.inflate(layoutInflater)
@@ -30,18 +38,39 @@ class KeyboardFragment : Fragment() {
             (binding.keysContainer.getChildAt(i) as KeyView).listener =
                 object : KeyView.OnKeyPressListener {
                     override fun onKeyDown(note: String) {
-                        // Encode note to message
-                        // Create note-on MidiMessage
-                        // midiSession.send(message)
+                        Log.d(TAG, "kEY dOWN")
+//                        sendMidiNote(note, true)
                     }
 
                     override fun onKeyUp(note: String) {
-                        // Encode note to message
-                        // Create note-off MidiMessage
-                        // midiSession.send(message)
+                        Log.d(TAG, "kEY uP")
+//                        sendMidiNote(note, false)
                     }
                 }
         }
         return binding.root
+    }
+
+    private fun sendMidiNote(note: String, isKeyDown: Boolean) {
+        val msg = encodeNote(note, isKeyDown)
+        midiSession.send(MidiMessage(msg, 0))
+    }
+
+    private fun encodeNote(noteString: String, isKeyDown: Boolean): ByteArray? {
+        val noteVal = (Note::name findBy noteString.uppercase(Locale.getDefault()))?.ordinal
+        noteVal?.let { note ->
+            val actualNote = note + (12 * octave)
+            val noteStatus = if (isKeyDown) 0x90 else 0x80
+            val buffer = ByteArray(3)
+            buffer[0] = (noteStatus + channel).toByte() // Note On/Off status
+            buffer[1] = actualNote.toByte()            // Pitch
+            buffer[2] = if (isKeyDown) 96 else 0      // Velocity (nbd for now)
+            return buffer
+        }
+        return null
+    }
+
+    companion object {
+        const val TAG = "KeyboardFragment"
     }
 }
