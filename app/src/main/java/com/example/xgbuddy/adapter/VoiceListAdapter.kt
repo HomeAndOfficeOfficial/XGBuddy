a08fd899396c1d63bbf2b7096a7c176aaa485b4c
apetersontrue <apeterson@truefitness.com>
2023-04-17 13:54:29 -0500

    Add voice and category selection logic

--- app/src/main/java/com/example/xgbuddy/adapter/VoiceListAdapter.kt
+++ app/src/main/java/com/example/xgbuddy/adapter/VoiceListAdapter.kt
@@ -1,17 +1,38 @@
 package com.example.xgbuddy.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.xgbuddy.R
+import com.example.xgbuddy.data.xg.XGNormalVoice
+import kotlin.reflect.full.isSubclassOf
 
 class VoiceListAdapter(
-    var voiceList: List<String>,
+    voiceEntries: List<Any>,
     private val listener: OnVoiceItemClickListener
 ) :
     RecyclerView.Adapter<VoiceListAdapter.ViewHolder>() {
+
+    private val voiceList: List<VoiceListEntry>
+    private var filteredList: List<VoiceListEntry>? = null
+
+    init {
+        voiceList = List(voiceEntries.size) {
+            VoiceListEntry(getVoiceName(voiceEntries[it]), voiceEntries[it]::class.java.name)
+        }
+    }
+
+    enum class VoiceListCategory(val enumName: String) {
+        XG_NORMAL(XGNormalVoice::class.java.name),
+        XG_DRUM("xgrum"),
+        SFX("sfx"),
+        QS300("qs300voice")
+    }
+
+    data class VoiceListEntry(val name: String, val typeName: String)
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -33,10 +54,30 @@ class VoiceListAdapter(
     override fun getItemCount(): Int = voiceList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(position, voiceList[position], listener)
+        filteredList?.let {
+            holder.bind(position, it[position].name, listener)
+        }
+    }
+
+    private fun getVoiceName(item: Any) = if (item.isEnum()) {
+        (item as Enum<*>).name
+    } else {
+        "Invalid Item"
+    }
+
+    private fun Any?.isEnum(): Boolean {
+        return this != null && this::class.isSubclassOf(Enum::class)
     }
 
     fun interface OnVoiceItemClickListener {
         fun onVoiceItemClicked(position: Int)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateCategory(category: VoiceListCategory) {
+        filteredList = voiceList.filter {
+            it.typeName == category.enumName
+        }
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/xgbuddy/ui/VoiceSelectionDialogFragment.kt
+++ app/src/main/java/com/example/xgbuddy/ui/VoiceSelectionDialogFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.core.view.children
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.xgbuddy.R
+import com.example.xgbuddy.adapter.VoiceListAdapter
+import com.example.xgbuddy.data.xg.XGNormalVoice
 import com.example.xgbuddy.databinding.FragmentVoiceSelectionDialogBinding
 
 class VoiceSelectionDialogFragment : DialogFragment() {
@@ -15,11 +21,77 @@ class VoiceSelectionDialogFragment : DialogFragment() {
         FragmentVoiceSelectionDialogBinding.inflate(layoutInflater)
     }
 
+    private lateinit var voiceListAdapter: VoiceListAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        initAdapter()
+        binding.rvVoiceSelect.apply {
+            adapter = voiceListAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+        setupCategoryButtons()
         return binding.root
+    }
+
+    private fun initAdapter() {
+        voiceListAdapter = VoiceListAdapter(
+            XGNormalVoice.values().toList() // TODO: Add other voice types
+        ) {
+            /**
+             * TODO: Not sure the best way to go about "selecting" the voice.
+             *  Probably need to wait until all the data structures are sorted out.
+             */
+            updateSelectedVoice(it)
+        }
+    }
+
+    private fun setupCategoryButtons() {
+        binding.rgVoiceCategory.setOnCheckedChangeListener { _, checkedId ->
+            updateAdapterCategory(checkedId)
+        }
+        setInitialCategory()
+    }
+
+    private fun setInitialCategory() {
+        val startCategoryId = arguments?.getInt(ARG_START_CATEGORY) ?: 0
+        var categorySelected = false
+        binding.rgVoiceCategory.children.forEach {
+            val isCategoryId = startCategoryId == it.id
+            (it as RadioButton).isChecked = isCategoryId
+            categorySelected = categorySelected || isCategoryId
+        }
+        if (!categorySelected) {
+            binding.rbXGVoice.isSelected = true
+        }
+    }
+
+    private fun updateAdapterCategory(categoryId: Int) {
+        val category = when (categoryId) {
+            CATEGORY_ID_NORMAL -> VoiceListAdapter.VoiceListCategory.XG_NORMAL
+            CATEGORY_ID_XGDRUM -> VoiceListAdapter.VoiceListCategory.XG_DRUM
+            CATEGORY_ID_SFX -> VoiceListAdapter.VoiceListCategory.SFX
+            CATEGORY_ID_QS300 -> VoiceListAdapter.VoiceListCategory.QS300 // Ugh, might have to make a complete list of QS300 voice presets
+            else -> null
+        }
+        category?.let { voiceListAdapter.updateCategory(it) }
+    }
+
+    private fun updateSelectedVoice(voiceIndex: Int) {
+        // TODO: Update viewmodel
+        dismiss()
+    }
+
+    companion object {
+        const val TAG = "VoiceSelectionDialogFragment"
+        const val ARG_START_CATEGORY = "argStartCategoryId"
+        const val ARG_START_VOICE = "argStartVoice"
+        const val CATEGORY_ID_NORMAL = R.id.rbXGVoice
+        const val CATEGORY_ID_XGDRUM = R.id.rbXGDrum
+        const val CATEGORY_ID_SFX = R.id.rbXGSfx
+        const val CATEGORY_ID_QS300 = R.id.rbQs300
     }
 }
