eeb8970976f30d73d77357387c22a9e800c2a8e4
apetersontrue <apeterson@truefitness.com>
2023-05-11 11:57:42 -0500

    Add stub for Effect base class

---
+++ app/src/main/java/com/example/xgbuddy/data/xg/Effect.kt
@@ -0,0 +1,45 @@
+package com.example.xgbuddy.data.xg
+
+import com.example.xgbuddy.util.EnumFinder.findBy
+
+data class Effect(
+    val nameRes: Int,
+    val msb: Byte,
+    val lsb: Byte,
+    val parameterList: Array<EffectParameter?>,
+    val effectType: EffectType
+) {
+
+//    var parameterValues: IntArray = getEffectDefaults()
+
+//    private fun getEffectDefaults(): ByteArray =
+//        when (effectType) {
+//            EffectType.REVERB -> (Reverb::nameRes findBy nameRes)!!.parameterDefaults
+//            EffectType.CHORUS -> (Chorus::nameRes findBy nameRes)!!.parameterDefaults
+//            EffectType.VARIATION -> (Variation::nameRes findBy nameRes)!!.parameterDefaults
+//        }
+
+    enum class EffectType { REVERB, CHORUS, VARIATION }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Effect
+
+        if (nameRes != other.nameRes) return false
+        if (msb != other.msb) return false
+        if (lsb != other.lsb) return false
+        if (!parameterList.contentEquals(other.parameterList)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = nameRes
+        result = 31 * result + msb
+        result = 31 * result + lsb
+        result = 31 * result + parameterList.contentHashCode()
+        return result
+    }
+}
