7b3271a4e631689d6f823f1a80832578dfc32fce
apetersontrue <apeterson@truefitness.com>
2023-05-31 15:16:53 -0500

    IP add some initialization logic

--- app/src/main/java/com/example/xgbuddy/ui/SystemFragment.kt
+++ app/src/main/java/com/example/xgbuddy/ui/SystemFragment.kt
@@ -5,12 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.xgbuddy.databinding.FragmentMidiSettingsBinding
+import com.example.xgbuddy.MidiSession
+import com.example.xgbuddy.databinding.FragmentSystemBinding
+import com.example.xgbuddy.ui.custom.ParameterControlView
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SystemFragment : Fragment() {
 
-    private val binding: FragmentMidiSettingsBinding by lazy {
-        FragmentMidiSettingsBinding.inflate(layoutInflater)
+    @Inject
+    lateinit var midiSession: MidiSession
+
+    private val binding: FragmentSystemBinding by lazy {
+        FragmentSystemBinding.inflate(layoutInflater)
     }
 
     override fun onCreateView(
@@ -18,6 +26,54 @@ class SystemFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        setupViewParams()
+        initControlListeners()
         return binding.root
     }
+
+    private fun setupViewParams() {
+        binding.apply {
+            scvMasterVol.apply {
+
+            }
+        }
+    }
+
+    private fun initControlListeners() {
+        binding.apply {
+            scvMasterVol.listener =
+                ParameterControlView.OnParameterChangedListener { controlParameter, isTouching ->
+
+                }
+            scvTranspose.listener =
+                ParameterControlView.OnParameterChangedListener { controlParameter, isTouching ->
+
+                }
+            scvMasterTune.listener =
+                ParameterControlView.OnParameterChangedListener { controlParameter, isTouching ->
+
+                }
+            bResetDrum.setOnClickListener(this@SystemFragment::resetDrums)
+            bResetParams.setOnClickListener(this@SystemFragment::resetParams)
+            bInitSetup.setOnClickListener(this@SystemFragment::resetSetup)
+            bPanic.setOnClickListener(this@SystemFragment::sendAllOff)
+        }
+    }
+
+    private fun resetDrums(v: View) {
+
+    }
+
+    private fun resetParams(v: View) {
+
+    }
+
+    private fun resetSetup(v: View) {
+
+    }
+
+    private fun sendAllOff(v: View) {
+
+    }
+
 }
