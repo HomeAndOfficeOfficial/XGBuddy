adfbc4a04134afd324885bb9f9d08c4553e5f617
subhouse07 <subhouse07@gmail.com>
2023-06-09 14:40:24 -0500

    Use basic seekbar for qs300 voice level

--- app/src/main/java/com/example/xgbuddy/ui/qs300/VoiceEditFragment.kt
+++ app/src/main/java/com/example/xgbuddy/ui/qs300/VoiceEditFragment.kt
@@ -4,24 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.example.xgbuddy.R
-import com.example.xgbuddy.data.ControlParameter
-import com.example.xgbuddy.data.qs300.QS300ControlParameter
+import com.example.xgbuddy.MidiSession
 import com.example.xgbuddy.data.qs300.QS300Element
-import com.example.xgbuddy.data.qs300.QS300VoiceParameter
 import com.example.xgbuddy.databinding.FragmentVoiceEditBinding
-import com.example.xgbuddy.ui.custom.ParameterControlView
+import com.example.xgbuddy.util.MidiMessageUtility
 import com.example.xgbuddy.viewmodel.QS300ViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class VoiceEditFragment : Fragment() {
+@AndroidEntryPoint
+class VoiceEditFragment : Fragment(), OnSeekBarChangeListener {
+
+    @Inject
+    lateinit var midiSession: MidiSession
 
     private val viewModel: QS300ViewModel by activityViewModels()
     private val binding: FragmentVoiceEditBinding by lazy {
         FragmentVoiceEditBinding.inflate(layoutInflater)
     }
+    private var isSeekbarUpdating = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,9 +35,7 @@ class VoiceEditFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         initObservers()
-        binding.cvVoiceLevel.apply {
-//            controlParameter =
-        }
+        binding.cvVoiceLevel.setOnSeekBarChangeListener(this)
         return binding.root
     }
 
@@ -39,6 +43,8 @@ class VoiceEditFragment : Fragment() {
         viewModel.preset.observe(viewLifecycleOwner) { preset ->
             preset?.voices!![viewModel.voice].let {
                 binding.etVoiceName.setText(it.voiceName)
+                isSeekbarUpdating = true
+                binding.cvVoiceLevel.progress = it.voiceLevel.toInt()
 //                cvVoiceLevel.value = it.voiceLevel
                 setElementContainerVisibilities(binding.llElementEditContainer, it.elements)
                 setElementContainerVisibilities(binding.llPrimaryControlContainer, it.elements)
@@ -57,5 +63,16 @@ class VoiceEditFragment : Fragment() {
                 View.VISIBLE
             }
         }
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {}
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+        val voiceLevel = seekBar!!.progress
+        val voice = viewModel.preset.value!!.voices[viewModel.voice]
+        voice.voiceLevel = voiceLevel.toByte()
+        midiSession.send(MidiMessageUtility.getQS300BulkDump(voice, viewModel.voice))
     }
 }
